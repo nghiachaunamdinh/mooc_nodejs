6c525c35839582413eb857001715edd7fc1d2799
nghiachaunamdinh <nghiachaunghiahung.com>
2021-10-09 09:57:27 +0700

    dac

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const path = require("path");
+const morgan = require('morgan')
+const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
-
+app.use(morgan("combined")) //HTTP logger
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+//console.log("PATH: ", __dirname)
+//app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
-        res.send('Hello World!')
+        //res.send('home')
+        res.render('home');
     })
     //ip 127.0.0.1-localhost
 app.listen(port, () => {
